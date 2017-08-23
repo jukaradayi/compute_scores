a7c521979ad7b9bb6fe4691af2935a9cd9c0e142
Juan Benjumea <jubenjum@gmail.com>
2017-08-23 14:14:42 +0200

    included gold class in compute_cov

--- compute_cov.py
+++ compute_cov.py
@@ -6,6 +6,7 @@ import logging
 import codecs
 from bisect import bisect_left, bisect_right, bisect
 from itertools import combinations, count
+from collections import defaultdict
 import argparse
 
 import numpy as np
@@ -19,7 +20,13 @@ try:
     PHON_GOLD=os.environ['PHON_GOLD']
 except:
     print("PHON_GOLD not set")
-    sys.exit
+    sys.exit()
+
+try:
+    CLASS_GOLD=os.environ['CLASS_GOLD']
+except:
+    print("CLASS_GOLD not set")
+    sys.exit()
 
 # if LOG environment doesnt exist then use the stderr
 try:
@@ -40,8 +47,8 @@ def cov_from_class(classes_file):
     '''compute the cov from the tde class file.'''
 
     ## reading the phoneme gold
-    phn_gold = PHON_GOLD
-    gold = read_gold_phn(phn_gold)
+    class_gold = CLASS_GOLD
+    gold, count_gold = read_gold_class(class_gold)
 
     # TODO : this code assume that the class file is build correctly but if not???
     logging.info("Parsing class file %s", classes_file)
@@ -87,14 +94,50 @@ def cov_from_class(classes_file):
             elif line[:5] == 'Class': # the class + number + ngram if available
                 pass
 
-            # getting the informati of the pairs
+            # getting the information of the pairs
             else:
                 fname, start, end = line.split(' ')
                 classes.append([fname, float(start), float(end)])
 
     # logging the results
-    cov_overall = count_overall.sum() / len(count_overall)
-    logging.info('overall: COV={:5.2f} elements={}'.format(cov_overall, n_overall))
+    cov_overall = np.sum(count_overall.astype('int') & count_gold.astype('int')) / count_gold.sum() 
+    #cov_overall = count_overall.sum() / n_phones 
+    logging.info('overall: COV={:.2f} elements={}'.format(cov_overall, n_overall))
+
+
+def read_gold_class(class_gold):
+    '''read the class gold file, it contains the gold tokens'''
+  
+    ## reading the phoneme gold 
+    phn_gold = PHON_GOLD 
+    gold = read_gold_phn(phn_gold) 
+
+    # create the counting vector. Gold tokens could covers less found phonemes   
+    n_phones = sum([len(gold[k]['start']) for k in gold.keys()])
+    count_gold = np.zeros(n_phones)
+
+    # decode class gold file and store intervals by speaker
+    tokens_by_spaker = defaultdict(list)
+    with codecs.open(class_gold, encoding='utf8') as cfile:
+        for lines in cfile:
+            line = lines.strip()
+            if line[:5] == 'Class' or len(line) == 0:
+                pass
+
+            else:
+                fname, start, end = line.split(' ')
+                tokens_by_spaker[fname].append([float(start), float(end)])
+    
+    # find all found fragments in gold and mark them in count_gold
+    for speaker in tokens_by_spaker.keys():
+        # search for intevals in the phoneme file                             
+        for interval in tokens_by_spaker[speaker]:
+            
+            b1_ = bisect_left(gold[speaker]['start'], interval[0])
+            e1_ = bisect_right(gold[speaker]['end'], interval[1]) 
+            count_gold[b1_:e1_] = 1
+
+    return gold, count_gold
 
 
 if __name__ == '__main__':
